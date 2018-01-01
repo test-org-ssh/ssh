0cb41e7c32c27b7f2d124096fe953a15b5e5e8bb
mamq <m603920757@outlook.com>
2018-01-01 19:48:50 +0800

    null

---
+++ src/com/onetoone/ssh/action/AdminLoginAction.java
@@ -0,0 +1,10 @@
+package com.onetoone.ssh.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AdminLoginAction extends ActionSupport{
+
+    public String login() {
+	return SUCCESS;
+    }
+}
